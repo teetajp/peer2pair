be1d7c284e3b81e6d70ec0f9866f96f1f98fe2c6
shmaiii <luongxuanmai2109@gmail.com>
2023-01-21 12:42:33 -0800

    backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// express json-parser
+app.use(express.json());
+
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
