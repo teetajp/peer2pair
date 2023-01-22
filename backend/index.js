062ab04ed89ebbf45c3d69dae84674d61290686f
TJ Pavaritpong <teetajp@gmail.com>
2023-01-21 16:56:17 -0800

    init new backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-// express json-parser
-app.use(express.json());
+// Express Route Definitions
+app.get("/", (req, res) => {
+    res.send("Hi");
+})
 
-
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(4000, (err) => {
+    console.log("Listening");
 })
