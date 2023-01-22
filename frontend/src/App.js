25c1c533c98c91d7e43ba192f44b8957cdca87b7
yasmeen45 <a.yasmeen3542@gmail.com>
2023-01-22 00:17:09 -0800

    listed fetched community info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import '@fontsource/roboto/300.css';
@@ -9,9 +8,8 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Community from './components/Community';
 import Theme from './components/Theme.js';
-import { useTheme } from '@mui/material/styles';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
+import { ThemeProvider, createTheme, useTheme } from '@mui/material/styles';
+import {Component} from "react"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const AppRoutes = () => {
@@ -22,25 +20,57 @@ const AppRoutes = () => {
   </BrowserRouter>
 }
 
-
-
 function DeepChild() {
   const theme = useTheme();
   return <span>{`spacing ${theme.spacing}`}</span>;
 }
 
+class App extends Component {
 
-function App() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://10.43.19.179:4000/communities')
+    .then(res => res.json())
+    .then(json => {
+      this.setState ({
+        isLoaded: true,
+        items:json,
+      })
+    })
+  }
+
+render() {
+  var { isLoaded, items } = this.state;
+
+  if(!isLoaded) {
+    return <p>Loading...</p>
+  }
+  else {
   return (
     <ThemeProvider theme={Theme}>
+      <ul>
+      {items.map(item => (
+        <li key={item.name}>
+          Name: {item.name}
+        </li>
+      ))}
+      </ul>
 
       <h1>Communities</h1>
       <Community/>
 
-      {/* <AppRoutes /> */}
+      <AppRoutes />
 
       </ThemeProvider>
-  )
+  )}
+}
 }
 
 export default App;
