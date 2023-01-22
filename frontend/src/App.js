0ea92d7e2631a7efe821b1368d327baf00d39dfa
yasmeen45 <a.yasmeen3542@gmail.com>
2023-01-21 23:46:52 -0800

    added theme - backgroun + text colour not loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Community from './components/Community';
+import Theme from './components/Theme.js';
+import { useTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -20,16 +23,23 @@ const AppRoutes = () => {
 }
 
 
+
+function DeepChild() {
+  const theme = useTheme();
+  return <span>{`spacing ${theme.spacing}`}</span>;
+}
+
+
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={Theme}>
 
       <h1>Communities</h1>
       <Community/>
 
-      <AppRoutes />
+      {/* <AppRoutes /> */}
 
-    </div>
+      </ThemeProvider>
   )
 }
 
