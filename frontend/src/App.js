83df8fe67c8afb7e577ebb86cffbb59accaf1cca
shmaiii <luongxuanmai2109@gmail.com>
2023-01-21 21:30:17 -0800

    react ver

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 import Community from './components/Community';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const AppRoutes = () => {
-  <BrowserRouter>
+  return(
     <Routes>
-      <Route path="/"></Route>
+      <Route path="/communities" element={<Community />}></Route>
     </Routes>
-  </BrowserRouter>
+    )
+ 
 }
 
 
