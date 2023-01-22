5e5bec221960bae0858cb8b1a41f305cfaa4a005
yasmeen45 <a.yasmeen3542@gmail.com>
2023-01-21 23:51:27 -0800

    added theme file

---
+++ frontend/src/components/Theme.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FD3A73',
+    },
+    secondary: {
+      main: '#424242',
+    },
+    background: {
+      default: '#d5d5d5',
+    },
+    text: {
+      primary: 'rgba(41,40,40,0.87)',
+      secondary: 'rgba(41,40,40,0.87)',
+    },
+  },
+});
+
+export default Theme;
+
