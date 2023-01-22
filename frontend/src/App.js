3b0c82aef156705eda4ab8d4bc51612d7df06e78
shmaiii <luongxuanmai2109@gmail.com>
2023-01-21 20:54:47 -0800

    app body

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,8 @@ function App() {
   return (
 
     <div>
-      <BrowserRouter>
+     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={}></Route>
-        <Route path="/joincommunity" element={}></Route> 
-        <Route path="login" element={<Login />}></Route>*/}
         <Route path="/communityfeed" element={<CommunityFeed />}></Route>
       </Routes >
       </BrowserRouter>
