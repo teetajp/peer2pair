b59f8be7466bbc8e6cdc8f8c095f2b050ed3b120
yasmeen45 <a.yasmeen3542@gmail.com>
2023-01-21 20:14:48 -0800

    basic dummy communities

---
+++ frontend/src/components/Community.js
@@ -0,0 +1,52 @@
+import react, { useEffect } from "react";
+import * as React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { margin } from "@mui/system";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(2),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
+
+const Item = (props) => {
+    return (
+    <Button href={props.link} variant="contained" sx={{
+        width: '100%', height: '70px', margin:'0px 15px 15px 0px',
+    }}>{props.name}</Button>
+    )
+}
+
+function Community() {
+    return (
+        <Box sx={{
+            width: '80%',
+            margin: '0 auto',
+        }}>
+            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item xs={6}>
+                    <Item name="Guitar" link="#"/>
+                </Grid>
+                <Grid item xs={6}>
+                    <Item name="Basketball" link="#"/>
+                </Grid>
+                <Grid item xs={6}>
+                    <Item name="Piano" link="#"/>
+                </Grid>
+                <Grid item xs={6}>
+                    <Item name="Swimming" link="#"/>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
+
+export default Community;
