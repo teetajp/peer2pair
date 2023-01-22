575817ed69e33f6625803969ab796c30809f6f35
shmaiii <luongxuanmai2109@gmail.com>
2023-01-22 00:22:06 -0800

    routing commit working version from ditch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import * as React from 'react';
 import Button from '@mui/material/Button';
 // import '@fontsource/roboto/300.css';
 // import '@fontsource/roboto/400.css';
 // import '@fontsource/roboto/500.css';
 // import '@fontsource/roboto/700.css';
 import Community from './components/Community';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
+import Header from "./components/Header";
+// import Footer from "./Components/Footer";
+// import About from "./Components/About";
+// import Resume from "./Components/Resume";
+// import Contact from "./Components/Contact";
+// import Portfolio from "./Components/Portfolio";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import {Routes, Route, Link} from "react-router-dom";
 
 const AppRoutes = () => {
   return(
@@ -18,18 +27,60 @@ const AppRoutes = () => {
  
 }
 
+class App extends Component {
 
-function App() {
-  return (
-    <div>
+  componentDidMount() {
+    this.getResumeData();
+  }
 
-      <h1>Communities</h1>
-      <Community/>
+  constructor(props) {
+    super(props);
+    this.state = {
+      foo: "bar",
+      resumeData: {}
+    };
 
-      <AppRoutes />
+    ReactGA.initialize("UA-110570651-1");
+    ReactGA.pageview(window.location.pathname);
+  }
 
-    </div>
-  )
+  getResumeData() {
+    $.ajax({
+      url: "./resumeData.json",
+      dataType: "json",
+      cache: false,
+      success: function(data) {
+        this.setState({ resumeData: data });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.getResumeData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path = "/" element = {<Header data={this.state.resumeData.main}></Header>}> </Route>
+          <Route path = "/SignIn" element = {<SignIn></SignIn>}> </Route>
+          <Route path = "/SignUp" element = {<SignUp></SignUp>}> </Route>
+        </Routes>
+
+       {/*  <Header data={this.state.resumeData.main} />
+        <About data={this.state.resumeData.main} />
+        <Resume data={this.state.resumeData.resume} />
+        <Portfolio data={this.state.resumeData.portfolio} />
+        <Contact data={this.state.resumeData.main} />
+        <Footer data={this.state.resumeData.main} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-   <App />
-  </BrowserRouter>
- ,
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
@@ -17,4 +16,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
